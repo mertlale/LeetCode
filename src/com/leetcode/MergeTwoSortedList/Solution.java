f4da7c91d05ae68e4e62d4829b15d17452a3e7fa
Mert Lale <mert.l@outlook.com>
2023-11-21 12:03:05 +0300

    clean code for MergeTwoSortedList

--- src/com/leetcode/MergeTwoSortedList/Solution.java
+++ src/com/leetcode/MergeTwoSortedList/Solution.java
@@ -18,44 +18,23 @@ class ListNode {
 }
 
 public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
         ListNode mergedListHead = new ListNode();
         ListNode current = mergedListHead;
-        System.out.println("\nNew Test");
 
-        while(l1 != null || l2 != null) {
-
-            if (l1 != null && l2 != null) { // if both is not empty
-                ListNode newNode = new ListNode();
-                current.next = newNode;
-                if (l1.val <= l2.val) { // if list1 is lower, take list1
-                    newNode.val = l1.val;
-                    l1 = l1.next;
-                } else { // if list2 is lower, take list2
-                    newNode.val = l2.val;
-                    l2 = l2.next;
-                }
-                current = current.next;
-            } else if (l1 != null) { // if list2 is empty
-                current.next = l1;
-                break;
+        while(list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
             }
-            else { // if list1 is empty
-                current.next = l2;
-                break;
-            }
-        }
-
-        return mergedListHead.next;
-    }
-
-    public static void printList(ListNode head) {
-        ListNode current = head;
-        while (current != null) {
-            System.out.print(current.val + " -> ");
             current = current.next;
         }
-        System.out.println("null");
+
+        current.next = list1 != null ? list1: list2;
+        return mergedListHead.next;
     }
 }
