9ba923d53efb227ddcf065435c40ad7ab87b69e6
Mert Lale <mert.l@outlook.com>
2023-11-23 18:35:15 +0300

    small edit for Solution 1 in ClimbingStairs

--- src/com/leetcode/ClimbingStairs/Solution.java
+++ src/com/leetcode/ClimbingStairs/Solution.java
@@ -11,18 +11,18 @@ Finally, it returns the value in the last cell of the DP table, which represents
 
 //public class Solution {
 //    public int climbStairs(int n) {
+//            if (n == 0 || n == 1) {
+//            return 1;
+//            }
 //
-//        int first = 1;
-//        int second = 1;
+//            int[] dp = new int[n+1];
+//            dp[0] = dp[1] = 1;
 //
-//        for (int i = 0; i < n-1; i++) {
-//            int temp = first;
-//            first = first + second;
-//            second = temp;
-//        }
-//
-//        return first;
-//    }
+//            for (int i = 2; i <= n; i++) {
+//            dp[i] = dp[i-1] + dp[i-2];
+//            }
+//            return dp[n];
+//            }
 //}
 
 /*
